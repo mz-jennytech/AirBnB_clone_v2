c7a52c26a73c7d52b9315fb4771ee49f2708d519
mz_jennytech <okosisijenifer@gmail.com>
2023-05-19 07:26:44 +0000

    config file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,11 +3,9 @@
 from flask import Flask
 """class Flask"""
 
-
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """displays text
     Returns:
@@ -15,6 +13,6 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
