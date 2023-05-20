b4ad0f7d345b716de0b0cdcce1a903d51511cbe5
mz_jennytech <okosisijenifer@gmail.com>
2023-05-20 02:20:57 +0000

    config file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""class Flask"""
+
 from flask import Flask
-"""class Flask"""
 
 app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """displays text
-    Returns:
-        text
-    """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
